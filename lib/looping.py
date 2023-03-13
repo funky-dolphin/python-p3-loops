f288bd72c9e5681b16d21e76839b282e1077bb9e
Nicholas Brereton <nicholasrbrereton@gmail.com>
2023-03-13 14:01:56 -0400

    lab complete

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i=10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
+    
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    int_list=[1,2,3,4,5]
+    squared_integers=[integer **2 for integer in int_list]
+    return squared_integers
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1,101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+ 
